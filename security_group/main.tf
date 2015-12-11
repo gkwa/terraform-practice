72ee0e4dfb2b0630800a771124df6c15b31e82ad
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-11 06:25:41 -0800

    whitespace

--- security_group/main.tf
+++ security_group/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
 }
 
 resource "aws_security_group" "allow_all" {
@@ -9,16 +9,16 @@ resource "aws_security_group" "allow_all" {
   description = "Allow all inbound traffic"
 
   ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }

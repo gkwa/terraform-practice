3f99e7d9e1df3f69d525c1e9f1fa7256f35190ec
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-11 07:24:40 -0800

    Add second sec group and attach to same vpc

--- security_group/main.tf
+++ security_group/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_security_group" "StreamboxLive-test" {
-  tags { Name = "StreamboxLive-test" }
-  name = "StreamboxLive-test"
+resource "aws_security_group" "StreamboxLive-test1" {
+  tags { Name = "StreamboxLive-test1" }
+  name = "StreamboxLive-test1"
   description = "Allow all inbound traffic"
   vpc_id = "vpc-981020fd"
 
@@ -23,4 +23,25 @@ resource "aws_security_group" "StreamboxLive-test" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+resource "aws_security_group" "StreamboxLive-test2" {
+  tags { Name = "StreamboxLive-test2" }
+  name = "StreamboxLive-test2"
+  description = "Allow all inbound traffic"
+  vpc_id = "vpc-981020fd"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }

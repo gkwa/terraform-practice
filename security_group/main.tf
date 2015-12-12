e4ffd7ef4e578b28227678e051d9e869c4b55218
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-11 21:06:25 -0800

    Go live with test VPC security group StreamboxLive
    This security group is really in use now.  Its associated with a
    pre-existing in production VPC.

--- security_group/main.tf
+++ security_group/main.tf
@@ -4,44 +4,102 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_security_group" "StreamboxLive-test1" {
-  tags { Name = "StreamboxLive-test1" }
-  name = "StreamboxLive-test1"
-  description = "Allow all inbound traffic"
-  vpc_id = "vpc-981020fd"
+resource "aws_security_group" "StreamboxLive" {
+  tags { Name = "StreamboxLive" }
+  name = "StreamboxLive"
+  description = "StreamboxLive"
+#  vpc_id = "vpc-981020fd"
+  vpc_id = "vpc-b3e71fd6"
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+	protocol = "icmp"
+	from_port = -1
+	to_port = -1
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+	from_port = 3389
+	to_port = 3389
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+	from_port = 80
+	to_port = 80
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+	from_port = 443
+	to_port = 443
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# actl3
+  ingress {
+	from_port = 1770
+	to_port = 1780
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# actl3
+  ingress {
+	from_port = 1770
+	to_port = 1780
+	protocol = "udp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# actl3
+  ingress {
+	from_port = 1900
+	to_port = 1900
+	protocol = "udp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# obfuscated ssh
+  ingress {
+	from_port = 6045
+	to_port = 6045
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# mysql
+  ingress {
+	from_port = 3306
+	to_port = 3306
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# streambox store and forward
+  ingress {
+	from_port = 2051
+	to_port = 2051
+	protocol = "udp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# streambox abn
+  ingress {
+	from_port = 2060
+	to_port = 2060
+	protocol = "udp"
+	cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-resource "aws_security_group" "StreamboxLive-test2" {
-  tags { Name = "StreamboxLive-test2" }
-  name = "StreamboxLive-test2"
-  description = "Allow all inbound traffic"
-  vpc_id = "vpc-981020fd"
-
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
   }
 
 }

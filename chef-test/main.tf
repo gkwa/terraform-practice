d1a5f39fa1cfa61e5de091043b45c83ecf986b71
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-01-10 17:33:16 -0800

    Open winrm for HTTPS in aws security group

--- chef-test/main.tf
+++ chef-test/main.tf
@@ -56,6 +56,13 @@ resource "aws_security_group" "win2008" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 5986
+    to_port = 5986
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
